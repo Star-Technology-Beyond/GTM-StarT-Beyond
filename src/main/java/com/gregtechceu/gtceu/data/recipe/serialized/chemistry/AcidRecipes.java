2fdf73eefc3da2cd1cb8c4aff7bf7f153dd06c22
256K Item Storage Cell <homlesskiddos@gmail.com>
2026-05-28 21:14:15 +1000

    Spotless formatting

--- src/main/java/com/gregtechceu/gtceu/data/recipe/serialized/chemistry/AcidRecipes.java
+++ src/main/java/com/gregtechceu/gtceu/data/recipe/serialized/chemistry/AcidRecipes.java
@@ -1,6 +1,7 @@
 package com.gregtechceu.gtceu.data.recipe.serialized.chemistry;
 
 import com.gregtechceu.gtceu.common.data.GTRecipeCategories;
+
 import net.minecraft.data.recipes.FinishedRecipe;
 
 import java.util.function.Consumer;
